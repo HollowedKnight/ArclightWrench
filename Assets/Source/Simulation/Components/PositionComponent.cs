640decc853365501a487df09a9d49053a51bea6e
Philipp Bornstedt <philipp.bornstedt@innogames.com>
2017-11-28 10:32:23 +0100

    Basic Entitas setup with simple components

---
+++ Assets/Source/Simulation/Components/PositionComponent.cs
@@ -0,0 +1,11 @@
+﻿using Entitas;
+using UnityEngine;
+
+namespace Source.Simulation.Components
+{
+    [Game]
+    public class PositionComponent : IComponent
+    {
+        public Vector3 Value;
+    }
+}
