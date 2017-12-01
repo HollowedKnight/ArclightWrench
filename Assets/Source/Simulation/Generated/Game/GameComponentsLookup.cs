b916dec4dc38a01d3e6c52aa5e4659fbd14df1fc
Philipp Bornstedt <philipp.bornstedt@innogames.com>
2017-12-01 11:26:57 +0100

    Created new game-related components

--- Assets/Source/Simulation/Generated/Game/GameComponentsLookup.cs
+++ Assets/Source/Simulation/Generated/Game/GameComponentsLookup.cs
@@ -8,15 +8,18 @@
 //------------------------------------------------------------------------------
 public static class GameComponentsLookup {
 
+    public const int Position = 0;
+    public const int SpawnActorCommand = 1;
 
-
-    public const int TotalComponents = 0;
+    public const int TotalComponents = 2;
 
     public static readonly string[] componentNames = {
-
+        "Position",
+        "SpawnActorCommand"
     };
 
     public static readonly System.Type[] componentTypes = {
-
+        typeof(PositionComponent),
+        typeof(SpawnActorCommandComponent)
     };
 }
